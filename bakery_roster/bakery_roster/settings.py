97f9419f967f901d2e743851a6945636e5121b15
JUNELAI2024 <159815232+JUNELAI2024@users.noreply.github.com>
2024-12-27 08:36:32 +0800

    update
    update

--- bakery_roster/bakery_roster/settings.py
+++ bakery_roster/bakery_roster/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-pzj%9p5!3+h7*%1^7@+luc2e%ef@bu(8)73n=m*k_q8%6&dwra
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
